018954ae88593ac6fa8085eecb058a38c9d5d2bb
glowcouch <42631203+glowcouch@users.noreply.github.com>
2026-05-02 17:08:45 +1200

    feat: set up ulstrasonic

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,39 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
-use embassy_time::Timer;
+use embassy_rp::gpio::{self, Input, Pull};
+use embassy_time::{Delay, Timer};
 use gpio::{Level, Output};
+use hcsr04::{Hcsr04, NoTemperatureCompensation};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+
     let mut led = Output::new(p.PIN_25, Level::Low);
 
-    loop {
-        info!("led on!");
-        led.set_high();
-        Timer::after_secs(1).await;
+    let trig = Output::new(p.PIN_0, Level::Low);
+    let echo = Input::new(p.PIN_1, Pull::None);
 
-        info!("led off!");
-        led.set_low();
-        Timer::after_secs(1).await;
+    let mut hcsr04 = Hcsr04::builder()
+        .trig(trig)
+        .echo(echo)
+        .delay(Delay)
+        .temperature(NoTemperatureCompensation)
+        .build();
+
+    led.set_high();
+
+    loop {
+        let dist = hcsr04.measure_distance().await.unwrap();
+
+        if dist > 10. {
+            led.set_low();
+        } else {
+            led.set_high();
+        }
+
+        Timer::after_millis(10).await;
     }
 }
